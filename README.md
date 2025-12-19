f2f2e9153ee000489707902b7265272afdd01c50
Lucía <100386238@alumnos.uc3m.es>
2025-12-19 01:11:43 +0100

    basic setup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # aesthetic-curriculum-accessibility-vitae
-Aesthetic &amp; Accessible Currucilum Vitaes.
+
+Aesthetic &amp; Accessible CVs.
---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/templates/**/*.{hbs,html}', './src/**/*.{ts,js}'],
+  theme: { extend: {} },
+  plugins: [],
+}
